a79f1598007c6c1f01566b647f74a9142d95a2ca
Jairo Matías <jairo_1829@hotmail.com>
2026-08-17 11:01:19 -0400

    test(pharmacy): import xUnit assertions

--- tests/MediCore.UnitTests/Pharmacy/MedicationTests.cs
+++ tests/MediCore.UnitTests/Pharmacy/MedicationTests.cs
@@ -1,4 +1,5 @@
 using MediCore.Domain.Pharmacy;
+using Xunit;
 
 namespace MediCore.UnitTests.Pharmacy;
 
@@ -8,10 +9,38 @@ public sealed class MedicationTests
     public void Medication_CanBeUpdatedAndReactivated()
     {
         var drugTypeId = Guid.NewGuid();
-        var medication = new Medication("MED-001", "Acetaminofén", "Paracetamol", "Paracetamol", "500 mg", "Tableta", "unidad", drugTypeId, null, null, false, false, null);
+        var medication = new Medication(
+            "MED-001",
+            "Acetaminofén",
+            "Paracetamol",
+            "Paracetamol",
+            "500 mg",
+            "Tableta",
+            "unidad",
+            drugTypeId,
+            null,
+            null,
+            false,
+            false,
+            null);
+
         medication.Deactivate();
-        medication.Update("MED-001", "Acetaminofén 500 mg", "Paracetamol", "Paracetamol", "500 mg", "Tableta", "unidad", drugTypeId, null, null, false, false, "Uso oral");
+        medication.Update(
+            "MED-001",
+            "Acetaminofén 500 mg",
+            "Paracetamol",
+            "Paracetamol",
+            "500 mg",
+            "Tableta",
+            "unidad",
+            drugTypeId,
+            null,
+            null,
+            false,
+            false,
+            "Uso oral");
         medication.Reactivate();
+
         Assert.True(medication.IsActive);
         Assert.Equal("Acetaminofén 500 mg", medication.Name);
         Assert.Equal("Uso oral", medication.Notes);
