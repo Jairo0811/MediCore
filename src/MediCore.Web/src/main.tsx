006f22e51e527f9e36d41742b5fa9b3681194d6a
Jairo Matías <jairo_1829@hotmail.com>
2026-09-14 19:32:25 -0400

    fix: load responsive UI overrides

--- src/MediCore.Web/src/main.tsx
+++ src/MediCore.Web/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './ui-enhancements.css';
+import './ui-responsive.css';
 import './accessibility.css';
 
 createRoot(document.getElementById('root')!).render(
